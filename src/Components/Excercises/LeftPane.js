c268511a58afc4983be8fc2664d4f43a437859ba
ajarulkn <ajarulkn@gmail.com>
2021-01-27 15:31:24 +0530

    Prepare basic page

---
+++ src/Components/Excercises/LeftPane.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Paper } from 'material-ui'
+
+const style = {
+  Paper: { padding: 20, marginTop: 10 }
+}
+export default (props) => <Paper style={style.Paper}>Left Pane</Paper>
--- src/Components/Layouts/Header.js
+++ src/Components/Layouts/Header.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import { AppBar, IconButton, Toolbar, Typography } from 'material-ui'
+import MenuIcon from 'material-ui/Menu'
 
 export default (props) => (
-  <div>
-    <h1>Header content</h1>
-  </div>
+  <AppBar position="static">
+    <Toolbar>
+      <IconButton edge="start" color="inherit">
+        <MenuIcon />
+      </IconButton>
+      <Typography variant="headline" color="inherit">
+        Excersie Database
+      </Typography>
+    </Toolbar>
+  </AppBar>
 )
